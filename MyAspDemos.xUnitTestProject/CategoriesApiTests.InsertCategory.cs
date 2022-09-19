f82268806702150a4795b149e45453bd41f857f5
Siva V <sivakumar26.v@gmail.com>
2022-09-19 13:33:19 +0530

    1.Changed Api CategoryController 2.Added New Project for Testing-Test MyAspDemos.xUnitTestProject 3.Testing Api Category for get,getid,put,post,update

---
+++ MyAspDemos.xUnitTestProject/CategoriesApiTests.InsertCategory.cs
@@ -0,0 +1,149 @@
+﻿using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using MyAspDemos.Controllers;
+
+using MyAspDemos.Models;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+using Xunit;
+
+
+
+
+namespace MyAspDemos.xUnitTestProject
+
+{
+
+    /// <remarks>
+
+    ///     Bad insertion data scenarios:
+
+    ///     - Name is NULL
+
+    ///     - Name is EMPTY STRING
+
+    ///     - Name contains more characters than what is allowed
+
+    ///     - NULL Category object
+
+    ///     
+
+    ///     LIMITATIONS OF WORKING WITH InMemory Database
+
+    ///     - Relationships between Tables are not supported.
+
+    ///     - EF Core DataAnnotation Validations are not supported.
+
+    /// </remarks>
+
+
+
+
+    public partial class CategoriesApiTests
+
+    {
+
+        [Fact]
+
+        public void InsertCategory_OkResult()
+
+        {
+
+            // ARRANGE
+
+            var dbName = nameof(CategoriesApiTests.InsertCategory_OkResult);
+
+            var logger = Mock.Of<ILogger<CategoriesController>>();
+
+            using var dbContext = DbContextMocker.GetApplicationDbContext(dbName);      // Disposable!
+
+            var apiController = new CategoriesController(dbContext, logger);
+
+            Category categoryToAdd = new Category
+
+            {
+
+                CategoryId = 5,
+
+                CategoryName = "New Category"             // IF = null, then: INVALID!  CategoryName is REQUIRED
+
+            };
+
+
+
+
+            // ACT
+
+            IActionResult actionResultPost = apiController.PostCategory(categoryToAdd).Result;
+
+
+
+
+            // ASSERT - check if the IActionResult is Ok
+
+            Assert.IsType<OkObjectResult>(actionResultPost);
+
+
+
+
+            // ASSERT - check if the Status Code is (HTTP 200) "Ok", (HTTP 201 "Created")
+
+            int expectedStatusCode = (int)System.Net.HttpStatusCode.OK;
+
+            var actualStatusCode = (actionResultPost as OkObjectResult).StatusCode.Value;
+
+            Assert.Equal<int>(expectedStatusCode, actualStatusCode);
+
+
+
+
+            // Extract the result from the IActionResult object.
+
+            var postResult = actionResultPost.Should().BeOfType<OkObjectResult>().Subject;
+
+
+
+
+            // ASSERT - if the result is a CreatedAtActionResult
+
+            Assert.IsType<CreatedAtActionResult>(postResult.Value);
+
+
+
+
+            // Extract the inserted Category object
+
+            Category actualCategory = (postResult.Value as CreatedAtActionResult).Value
+
+                                      .Should().BeAssignableTo<Category>().Subject;
+
+
+
+
+            // ASSERT - if the inserted Category object is NOT NULL
+
+            Assert.NotNull(actualCategory);
+
+
+
+
+            Assert.Equal(categoryToAdd.CategoryId, actualCategory.CategoryId);
+
+            Assert.Equal(categoryToAdd.CategoryName, actualCategory.CategoryName);
+
+        }
+
+    }
+
+}
--- MyAspDemos/Controllers/CategoriesController.cs
+++ MyAspDemos/Controllers/CategoriesController.cs
@@ -5,43 +5,98 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using MyAspDemos.Data;
 using MyAspDemos.Models;
 
 namespace MyAspDemos.Controllers
 {
+    /// <remarks>
+    ///     In an ASP.NET Core REST API, there is no need to explicitly check if the model state is Valid. 
+    ///     Since the controller class is decorated with the [ApiController] attribute, 
+    ///     it takes care of checking if the model state is valid 
+    ///     and automatically returns 400 response along the validation errors.
+    ///     Example response:
+    ///         {
+    ///             "type": "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+    ///             "title": "One or more validation errors occurred.",
+    ///             "status": 400,
+    ///             "traceId": "|65b7c07c-4323622998dd3b3a.",
+    ///             "errors": {
+    ///                 "Email": [
+    ///                     "The Email field is required."
+    ///                 ],
+    ///                 "FirstName": [
+    ///                     "The field FirstName must be a string with a minimum length of 2 and a maximum length of 100."
+    ///                 ]
+    ///             }
+    ///         }
+    /// </remarks>
+
+
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<CategoriesController> _logger;
 
-        public CategoriesController(ApplicationDbContext context)
+        public CategoriesController(
+            ApplicationDbContext context,
+            ILogger<CategoriesController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: api/Categories
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        // public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        public async Task<IActionResult> GetCategories()
         {
-            return await _context.Categories
-                                 .Include(c => c.Books)
-                                 .ToListAsync();
+            try
+            {
+                var categories = await _context.Categories
+                                     .Include(c => c.Books)
+                                     .ToListAsync();
+
+                // Check if data exists in the Database
+                if (categories == null)
+                {
+                    return NotFound();          // RETURN: No data was found            HTTP 404
+                }
+                return Ok(categories);          // RETURN: OkObjectResult - good result HTTP 200
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message); // RETURN: BadResult                    HTTP 400
+            }
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Category>> GetCategory(int id)
+        public async Task<IActionResult> GetCategory(int? id)
         {
-            var category = await _context.Categories.FindAsync(id);
-
-            if (category == null)
+            if (!id.HasValue)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            return category;
+            try
+            {
+                var category = await _context.Categories.FindAsync(id);
+
+                if (category == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(category);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message);
+            }
         }
 
         // PUT: api/Categories/5
@@ -94,7 +149,7 @@ namespace MyAspDemos.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Category>> PostCategory(Category category)
+        public async Task<IActionResult> PostCategory(Category category)
         {
             // Sanitize the Data
             category.CategoryName = category.CategoryName.Trim();
@@ -130,8 +185,13 @@ namespace MyAspDemos.Controllers
 
         // DELETE: api/Categories/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Category>> DeleteCategory(int id)
+        public async Task<IActionResult> DeleteCategory(int? id)
         {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+
             try
             {
                 var category = await _context.Categories.FindAsync(id);
